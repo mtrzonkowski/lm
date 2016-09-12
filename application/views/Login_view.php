980098b7c482e1a5b5490458951ce67e4cc1139d
Michał <Micha@192.168.1.14>
2016-09-12 08:00:00 +0200

    rano

---
+++ application/views/Login_view.php
@@ -0,0 +1,21 @@
+<?php include '/application/views/basic/head.php'; ?>
+<form method="POST">
+    <div class="row">
+        <div class="col l2 m6 s12">
+        <input type="text" name="login" />
+        <input type="password" name="pass"/>
+        <button type="submit" class="btn">
+
+        </button>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+</form>
+<?php include '/application/views/basic/foot.php'; ?>
---
+++ application/views/basic/head.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Lemi</title>
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+        <link type="text/css" rel="stylesheet" href="/application/libraries/css/materialize.clockpicker.css"  media="screen,projection"/>   
+    </head>
+    <body>
+
+        <div id="container">
+            <div id="body">
